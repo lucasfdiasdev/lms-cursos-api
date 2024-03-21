f32b91d07e02318c350571f3d8b3993151c5791b
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-21 15:44:52 -0300

    feat: create authorizes roles

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -45,3 +45,17 @@ export const isAuthenticated = CatchAsyncError(
     next();
   }
 );
+
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          403,
+          `Role: ${req.user?.role} is not allowed to access this resource`
+        )
+      );
+    }
+    next();
+  };
+};
