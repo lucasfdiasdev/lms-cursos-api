d98dc195bb8b4522cf9d14cafbea35037b34caf6
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-21 19:02:28 -0300

    refactor: interface types CourseModel

--- src/models/course.model.ts
+++ src/models/course.model.ts
@@ -1,52 +1,11 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
-
-interface IComment extends Document {
-  user: object;
-  comment: string;
-  commentReplies: IComment[];
-}
-
-interface IReview extends Document {
-  user: object;
-  rating: number;
-  comment: string;
-  commentReplies: IComment[];
-}
-
-interface ILink extends Document {
-  title: string;
-  url: string;
-}
-
-interface ICourseData extends Document {
-  title: string;
-  description: string;
-  videoUrl: string;
-  videoThumnail: string;
-  videoSection: string;
-  videoLength: number;
-  videoPlayer: string;
-  links: ILink[];
-  suggestion: string;
-  questions: IComment[];
-}
-
-interface ICourse extends Document {
-  name: string;
-  description: string;
-  price: number;
-  estimatedPrice?: number;
-  thumbnail: object;
-  tags: string[];
-  level: string;
-  demoUrl: string;
-  benefits: { title: string }[];
-  prerequisites: { title: string }[];
-  reviews: IReview[];
-  courseData: ICourseData[];
-  ratings?: number;
-  purchased?: number;
-}
+import mongoose, { Model, Schema } from "mongoose";
+import {
+  ILink,
+  ICourse,
+  IReview,
+  IComment,
+  ICourseData,
+} from "../repositories/course.repository";
 
 const reviewSchema = new Schema<IReview>({
   user: Object,
---
+++ src/repositories/course.repository.ts
@@ -0,0 +1,49 @@
+import { Document } from "mongoose";
+
+export interface IComment extends Document {
+  user: object;
+  comment: string;
+  commentReplies: IComment[];
+}
+
+export interface IReview extends Document {
+  user: object;
+  rating: number;
+  comment: string;
+  commentReplies: IComment[];
+}
+
+export interface ILink extends Document {
+  title: string;
+  url: string;
+}
+
+export interface ICourseData extends Document {
+  title: string;
+  description: string;
+  videoUrl: string;
+  videoThumnail: string;
+  videoSection: string;
+  videoLength: number;
+  videoPlayer: string;
+  links: ILink[];
+  suggestion: string;
+  questions: IComment[];
+}
+
+export interface ICourse extends Document {
+  name: string;
+  description: string;
+  price: number;
+  estimatedPrice?: number;
+  thumbnail: object;
+  tags: string[];
+  level: string;
+  demoUrl: string;
+  benefits: { title: string }[];
+  prerequisites: { title: string }[];
+  reviews: IReview[];
+  courseData: ICourseData[];
+  ratings?: number;
+  purchased?: number;
+}
