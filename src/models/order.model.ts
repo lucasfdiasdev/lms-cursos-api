bb249b8e470e55c8093231696d827add51b1ecbb
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-22 15:48:45 -0300

    feat: create order entity

---
+++ src/models/order.model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { IOrder } from "../repositories/order.respoitory";
+
+const orderSchema = new Schema<IOrder>(
+  {
+    courseId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    payment_info: {
+      type: Object,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const orderModel: Model<IOrder> = mongoose.model("Order", orderSchema);
